a7b3a49e3cdfe368d4664ffecb45fb0414bf3b21
prasadtelkikar <telkikar.prasad@gmail.com>
2017-03-22 21:20:57 +0530

    Solved 2 easy problems from Hackerrank

---
+++ Algorithms/Implementations/BreakingTheRecords.cs
@@ -0,0 +1,54 @@
+﻿/*Thank you Sung Kim for your help @http://www.slightedgecoder.com/ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingTheRecords
+{
+    /*Sample Input 1:
+     * 9
+     * 10 5 20 20 4 5 2 25 1
+     *
+     *Sample output 1:
+     * 2 4
+     */
+
+    /*Sample Input 1:
+     * 10
+     * 3 4 21 36 10 28 35 5 24 42
+     * 
+     *Sample output 1:
+     * 4 0
+     */
+
+    class BreakingTheRecords
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] score_temp = Console.ReadLine().Split(' ');
+            int[] score = Array.ConvertAll(score_temp, Int32.Parse);
+            int highestRecordBreakCount = 0, worstRecordBreakCount = 0;
+            int hightestRecord = score[0];
+            int worstRecord = score[0];
+            for (int i = 1; i < n; i++)
+            {
+                if (hightestRecord < score[i])
+                {
+                    hightestRecord = score[i];
+                    highestRecordBreakCount++;
+                }
+                if (worstRecord > score[i])
+                {
+                    worstRecord = score[i];
+                    worstRecordBreakCount++;
+                }
+            }
+            Console.WriteLine(highestRecordBreakCount+" "+worstRecordBreakCount);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithms/Implementations/RepeatedString.cs
@@ -0,0 +1,53 @@
+﻿/*Thank you Sung Kim for your help @http://www.slightedgecoder.com/ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingTheRecords
+{
+    class RepeatedString
+    {
+        /* Sample Input 0:
+         * aba
+         * 10
+         * 
+         * Sample output 0:
+         * 7
+         */
+        /*
+         * Sample Input 1:
+         * a
+         * 1000000000000
+         * 
+         * Sample output 1:
+         * 1000000000000
+         */
+
+        public static void Main(string[] args)
+        {
+
+            long remainder = 0, numberOfa = 0, quotient = 0, remainderCount = 0;
+            string infiniteString = Console.ReadLine();
+            long n = Convert.ToInt64(Console.ReadLine());
+            remainder = n % infiniteString.Length;
+            quotient = n / infiniteString.Length;
+
+            foreach(char ch in infiniteString)
+            {
+                if (ch == 'a')
+                    numberOfa++;
+            }
+
+            for (int i = 0; i < remainder; i++)
+            {
+                if (infiniteString[i] == 'a')
+                    remainderCount++;
+            }
+
+            Console.WriteLine(((quotient * numberOfa) + remainderCount));
+            Console.ReadKey();
+        }
+    }
+}
