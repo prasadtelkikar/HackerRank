888e70e20b621e8f5cf9f3fd3b75a1f133d6b325
prasadtelkikar <telkikar.prasad@gmail.com>
2017-03-25 10:25:28 +0530

    Added logic

--- Algorithms/Implementations/Cheated/DrawingBook.cs
+++ Algorithms/Implementations/Cheated/DrawingBook.cs
@@ -17,11 +17,25 @@ using System.Threading.Tasks;
  */
 namespace Implementations.Cheated
 {
+    /// <summary>
+    /// Drawing Book: Solved after looking into Discussion board. Simplest way to solve this kind of problems
+    /// https://www.hackerrank.com/challenges/drawing-book
+    /// </summary>
     class DrawingBook
     {
         public static void Main(String[] args)
         {
-            
+            int lastPage = Convert.ToInt32(Console.ReadLine());
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+            int pageTurnCount = GetMinPageTurns(lastPage, pageNumber);
+            Console.WriteLine(pageTurnCount);
+        }
+
+        private static int GetMinPageTurns(int lastPage, int pageNumber)
+        {
+            int pageTurnCountFromStart = (int)Math.Floor(pageNumber/2.0);
+            int pageTurnCountFromLast = (int) Math.Floor((lastPage - pageNumber)/2.0);
+            return Math.Min(pageTurnCountFromStart, pageTurnCountFromLast);
         }
     }
 }
