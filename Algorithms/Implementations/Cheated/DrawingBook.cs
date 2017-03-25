393985afb038d99e5d814a7fd82fea1088f73dbc
prasadtelkikar <telkikar.prasad@gmail.com>
2017-03-25 10:22:19 +0530

    Solved Drawing book problem in cheated folder

---
+++ Algorithms/Implementations/Cheated/DrawingBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Sample Input 0:
+ * 6
+ * 2
+ * Sample output 0:
+ * 1
+ */
+/*Sample Input 1:
+ * 5
+ * 4
+ * Sample output 1:
+ * 0
+ */
+namespace Implementations.Cheated
+{
+    class DrawingBook
+    {
+        public static void Main(String[] args)
+        {
+            
+        }
+    }
+}
