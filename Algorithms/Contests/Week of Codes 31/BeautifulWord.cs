3ad274df2029bf15d3a88b638de499ac39cc4d24
prasadtelkikar <telkikar.prasad@gmail.com>
2017-04-10 22:58:59 +0530

    Solved first problem from Week of codes 31. All test cases pass

---
+++ Algorithms/Contests/Week of Codes 31/BeautifulWord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contests.Week_of_Codes_31
+{
+    class BeautifulWord
+    {
+        public static void Main()
+        {
+            char[] vowels = {'a', 'e', 'i', 'o', 'u', 'y'};
+            string word = Console.ReadLine();
+            bool flag = true;
+            for (int i = 0; i < (word.Length -1); i++)
+                if (word[i] == word[i + 1] || (vowels.Contains(word[i])) && vowels.Contains(word[i + 1]))
+                    flag = false;
+
+            Console.WriteLine(flag ? "Yes" : "No");
+            Console.ReadKey();
+        }
+    }
+}
