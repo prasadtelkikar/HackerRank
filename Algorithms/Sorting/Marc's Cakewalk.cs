f9de479c748d269e93a6b0eb5024c1337aa00c97
prasadtelkikar <telkikar.prasad@gmail.com>
2017-04-22 22:38:52 +0530

    Solved 2 programs

---
+++ Algorithms/Sorting/Marc's Cakewalk.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Sorting
+{
+/*Sample input 0:
+ * 3
+ * 1 3 2
+ * Sample output 0:
+ * 11
+ */
+    class Marc_s_Cakewalk
+    {
+        /// <summary>
+        /// Marc's Cakewalk
+        /// https://www.hackerrank.com/challenges/marcs-cakewalk
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(String[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            string[] arrString = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arrString, Int32.Parse);
+            Array.Sort(arr, (a, b) => b.CompareTo(a));
+            double calories = 0;
+            for (int i = 0; i < size; i++)
+            {
+                double result = arr[i] * Math.Pow(2.0, i);
+                calories += result;
+            }
+            Console.WriteLine(calories);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithms/Sorting/Quicksort-1-Partition.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Sample input 0:
+ * 5
+ * 4 5 3 7 2
+ * Sample output 0:
+ * 3 2 4 5 7
+ */
+namespace Sorting
+{
+    /// <summary>
+    /// Quick sort partition: Solved as per description
+    /// https://www.hackerrank.com/challenges/quicksort1
+    /// </summary>
+    class Quicksort_1_Partition
+    {
+        public static void Main(String[] args)
+        {
+            int _ar_size;
+            _ar_size = Convert.ToInt32(Console.ReadLine());
+            int[] _ar = new int[_ar_size];
+            String elements = Console.ReadLine();
+            String[] split_elements = elements.Split(' ');
+            for (int _ar_i = 0; _ar_i < _ar_size; _ar_i++)
+            {
+                _ar[_ar_i] = Convert.ToInt32(split_elements[_ar_i]);
+            }
+
+            Partition(_ar);
+            Console.ReadKey();
+        }
+
+        private static void Partition(int[] ar)
+        {
+            int p = ar[0];
+            List<int> arrLeft = new List<int>();
+            List<int> arrRight = new List<int>();
+            for (int i = 1; i < ar.Length; i++)
+            {
+                if (ar[i] < p)
+                    arrLeft.Add(ar[i]);
+                else if (ar[i] > p)
+                    arrRight.Add(ar[i]);
+            }
+            foreach (int element in arrLeft)
+                Console.Write(element + " ");
+
+            Console.Write(p + " ");
+            foreach (int element in arrRight)
+                Console.Write(element + " ");
+        }
+    }
+}
