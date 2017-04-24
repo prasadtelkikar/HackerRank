f5aa5f1dd139a02271b8bced72eacd3b8ed4d3a6
prasadtelkikar <telkikar.prasad@gmail.com>
2017-04-24 23:09:52 +0530

    Solved one easy program from Sorting brance

---
+++ Algorithms/Sorting/AbsoluteDifference.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class AbsoluteDifference
+    {
+        public static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            string[] arrString = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arrString, Int32.Parse);
+            int minValue = Int32.MaxValue;
+            Array.Sort(arr);
+            for (int i = 0; i < size - 1; i++)
+            {
+                int result = Math.Abs(arr[i] - arr[i + 1]);
+                if (minValue > result)
+                    minValue = result;
+            }
+            Console.WriteLine(minValue);
+            Console.ReadKey();
+        }
+    }
+}
