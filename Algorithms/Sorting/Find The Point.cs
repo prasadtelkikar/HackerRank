177ba7eed06bb574aa9085541fc5d5b99468a191
prasadtelkikar <telkikar.prasad@gmail.com>
2017-05-04 00:00:56 +0530

    Solved one easy program from Hackerrank sorting section; one is still incomplete

---
+++ Algorithms/Sorting/Find The Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Find_The_Point
+    {
+        public static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            float[] nextX = new float[testCases];
+            float[] nextY = new float[testCases];
+            for (int i = 0; i < testCases; i++)
+            {
+                string[] pointsString = Console.ReadLine().Split(' ');
+                float[] points = Array.ConvertAll(pointsString, float.Parse);
+
+                float diffX = points[2] - points[0];
+                float diffY = points[3] - points[1];
+
+                nextX[i] = points[2] + diffX;
+                nextY[i] = points[3] + diffY;
+            }
+            for (int i = 0; i < testCases; i++)
+            {
+                Console.WriteLine(nextX[i] +" "+nextY[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithms/Sorting/Insertion Sort - Part 1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Insertion_Sort___Part_1
+    {
+        public static void Main(string[] args)
+        {
+            int arrSize = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[arrSize];
+            string[] elements = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < arrSize; i++)
+                arr[i] = Convert.ToInt32(elements[i]);
+
+            InsertionSort(arr);
+
+        }
+
+        private static void InsertionSort(int[] arr)
+        {
+            int size = arr.Length;
+            for (int i = size - 1; i >= 0; i--)
+            {
+                int key = arr[i];
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (key < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        break;
+                    }
+                        
+                }
+                DisplayArray(arr);
+                Console.WriteLine();
+                
+            }
+            Console.ReadKey();
+        }
+
+        private static void DisplayArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
