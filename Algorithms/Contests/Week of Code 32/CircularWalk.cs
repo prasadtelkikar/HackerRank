d2dadeca9a9e440eb65b5b53bd4443fb09dbfd47
prasadtelkikar <telkikar.prasad@gmail.com>
2017-05-22 15:23:12 +0530

    Week of code 32: one failed and one passed with 12.5 points

---
+++ Algorithms/Contests/Week of Code 32/CircularWalk.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contests.Week_of_Code_32
+{
+    class CircularWalk
+    {
+        public static void Main(String[] args)
+        {
+            string[] firstLine = Console.ReadLine().Split(' ');
+            string[] secondLine = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(firstLine[0]);
+            int s = Convert.ToInt32(firstLine[1]);
+            int t = Convert.ToInt32(firstLine[2]);
+
+            int[] r = new int[n];
+            r[0] = Convert.ToInt32(secondLine[0]);
+            int g = Convert.ToInt32(secondLine[1]);
+            int seed = Convert.ToInt32(secondLine[2]);
+            int p = Convert.ToInt32(secondLine[3]);
+
+            for (int i = 1; i < n; i++)
+                r[i] = ((r[i - 1]*g) + seed)%p;
+
+            int initial = s;
+            while (true)
+            {
+                int clockwise = s + r[initial];
+                if (clockwise >= n)
+                    clockwise %= n;
+                int anticlockwise = s - r[initial];
+                if (anticlockwise < 0)
+                {
+                    anticlockwise = n - anticlockwise;
+                }
+                //if(r[initial])
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithms/Contests/Week of Code 32/GeometricTrick.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contests.Week_of_Code_32
+{
+    public class GeometricTrick
+    {
+        public static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            string charArray = Console.ReadLine();
+            List<int> indexA = new List<int>();
+            List<int> indexB = new List<int>();
+            List<int> indexC = new List<int>();
+
+            for (int i = charArray.IndexOf('a'); i > -1 && i < size; i = charArray.IndexOf('a', i+1))
+            {
+                indexA.Add(i + 1);
+            }
+            for (int i = charArray.IndexOf('b'); i > -1; i = charArray.IndexOf('b', i + 1))
+            {
+                int temp = (int)Math.Pow((i + 1), 2);
+                indexB.Add(temp);
+            }
+            for (int i = charArray.IndexOf('c'); i > -1; i = charArray.IndexOf('c', i + 1))
+            {
+                indexC.Add(i + 1);
+            }
+            int count = 0;
+            
+            foreach (int b in indexB)
+            {
+                foreach (int a in indexA)
+                {
+                    if (b%a == 0)
+                    {
+                        int result = b / a;
+                        if (indexC.Contains(result))
+                            count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
+
