d71d8de84887150a090467264457e187d10a2243
prasadtelkikar <telkikar.prasad@gmail.com>
2017-06-10 23:53:12 +0530

    Solved SparseArray problem with O(n^2) complexity

---
+++ Algorithms/Arrays/SparseArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class SparseArray
+    {
+        public static void Main(string[] args)
+        {
+            int sizeN = Convert.ToInt32(Console.ReadLine());
+            List<string> inputStrings = new List<string>();
+            for (int i = 0; i < sizeN; i++)
+                inputStrings.Add(Console.ReadLine());
+
+            int sizeQ = Convert.ToInt32(Console.ReadLine());
+            List<string> findString = new List<string>();
+
+            for (int i = 0; i < sizeQ; i++)
+                findString.Add(Console.ReadLine());
+
+            foreach (string fstring in findString)
+            {
+                int count = 0;
+                foreach (string iString in inputStrings)
+                {
+                    if (iString.Equals(fstring))
+                        count++;
+                }
+                Console.WriteLine(count);
+            }
+            Console.ReadKey();
+        }
+    }
+}
