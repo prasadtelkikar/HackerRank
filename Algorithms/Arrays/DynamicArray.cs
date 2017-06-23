7ad54d054930a74bc07731057bdfc4cd33df6319
prasadtelkikar <telkikar.prasad@gmail.com>
2017-06-23 23:25:53 +0530

    DynamicArray half done; sample test case executing successfully

--- Algorithms/Arrays/DynamicArray.cs
+++ Algorithms/Arrays/DynamicArray.cs
@@ -1,50 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Arrays
-{
-    class DynamicArray
     {
-        public static void Main(string[] args)
+    class DynamicArray
         {
-            Dictionary<int, List<int>> seqList = new Dictionary<int, List<int>>();
-            int lastAnswer = 0;
-            string[] nq = Console.ReadLine().Split(' ');
+        public static void Main (string[] args)
+        {
+            string[] file = File.ReadAllLines(@"C:\Users\Prasad.Telkikar\Desktop\Docs\Input.txt");
+            string[] nq = file[0].Split(' ');//Console.ReadLine().Split(' ');
+            int emptySequences = Convert.ToInt32(nq[0]);
+            int numberOfQueries = Convert.ToInt32(nq[1]);
 
-            int N = Convert.ToInt32(nq[0]);
-            int Q = Convert.ToInt32(nq[1]);
-            for (int i = 0; i < N; i++)
-            {
-                List<int> emptyLinkedList = new List<int>();
-                seqList.Add(i, emptyLinkedList);
-            }
-            List<int> lastAnsList = new List<int>();
-            for (int i = 0; i < Q; i++)
-            {
-                string[] line = Console.ReadLine().Split(' ');
-                int queryType = Convert.ToInt32(line[0]);
-                int x = Convert.ToInt32(line[1]);
-                int y = Convert.ToInt32(line[2]);
+            long[][] seqList = new long[emptySequences][];
+            long lastAnswer = 0;
+            List<long> lastAnswerlist = new List<long>();
+            string[] queries = new string[numberOfQueries];
 
-                int index = (x ^ lastAnswer)%N;
-                if(queryType == 1)
-                    seqList[index].Add(y);
-                if ( queryType == 2 )
+            //for ( int i = 0; i < numberOfQueries; i++ )
+            //    {
+            //    queries[i] = Console.ReadLine();
+            //    }
+
+            long lastIndex = 0;
+            int index = 0;
+            foreach ( string query in file )
                 {
-                    int length = y%(seqList.Count);
-                    IList<int> list = seqList[index];
-                    lastAnswer = list[length];
-                    lastAnsList.Add(lastAnswer);
+                if ( index == 0 )
+                    {
+                    index++;
+                    continue;
+                    }
+                index++;
+                string[] splitQuery = query.Split(' ');
+                int queryType = Convert.ToInt32(splitQuery[0]);
+                long x = Convert.ToInt64(splitQuery[1]);
+                long y = Convert.ToInt64(splitQuery[2]);
+
+                long commonTerm = ((x ^ lastAnswer) % emptySequences);
+                if ( queryType == 1 )
+                {
+                    long[] seq = seqList[commonTerm];
+                    if ( seq != null )
+                        lastIndex = seq.Length - 1;
+                    else
+                        {
+                        seqList[commonTerm] = new long[emptySequences];
+                        }
+                    seqList[commonTerm][lastIndex] = y;
+                    }
+                else
+                    {
+                    long[] seq = seqList[commonTerm];
+                    if(seq != null)
+                    lastIndex = y % seq.Length;
+                    lastAnswerlist.Add(seq[lastIndex]);
+                    lastAnswer = seq[lastIndex];
+                    }
                 }
+            foreach (var value in lastAnswerlist)
+                Console.WriteLine(value);
+
+            Console.ReadLine();
             }
-            foreach (var entity in lastAnsList)
-            {
-                Console.WriteLine(entity);
-            }
-            Console.ReadKey();
         }
     }
-}
