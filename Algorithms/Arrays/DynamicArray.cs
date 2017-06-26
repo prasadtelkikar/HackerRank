a6555784abc307697a4f21625183a523dfebae76
prasadtelkikar <telkikar.prasad@gmail.com>
2017-06-26 22:39:28 +0530

    Solved dynamic array from array section

--- Algorithms/Arrays/DynamicArray.cs
+++ Algorithms/Arrays/DynamicArray.cs
@@ -1,67 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-
+/* Sample input 0:
+ * 2 5
+ * 1 0 5
+ * 1 1 7
+ * 1 0 3
+ * 2 1 0
+ * 2 1 1
+ * Sample output 0:
+ * 7
+ * 3
+ */
 namespace Arrays
     {
+    /// <summary>
+    /// Dynamic array
+    /// https://www.hackerrank.com/challenges/dynamic-array/problem
+    /// Note: Used Dictionary. Not array; try to solve it with array
+    /// </summary>
     class DynamicArray
         {
         public static void Main (string[] args)
-        {
-            string[] file = File.ReadAllLines(@"C:\Users\Prasad.Telkikar\Desktop\Docs\Input.txt");
-            string[] nq = file[0].Split(' ');//Console.ReadLine().Split(' ');
-            int emptySequences = Convert.ToInt32(nq[0]);
-            int numberOfQueries = Convert.ToInt32(nq[1]);
+            {
+            //string[] lines = File.ReadAllLines(@"D:\GitHub\HackerRank\Algorithms\Arrays\InputsAndOutputs\Input.txt");
+            //string[] nq = lines[0].Split(' ');
 
-            long[][] seqList = new long[emptySequences][];
-            long lastAnswer = 0;
-            List<long> lastAnswerlist = new List<long>();
-            string[] queries = new string[numberOfQueries];
-
-            //for ( int i = 0; i < numberOfQueries; i++ )
-            //    {
-            //    queries[i] = Console.ReadLine();
-            //    }
-
-            long lastIndex = 0;
-            int index = 0;
-            foreach ( string query in file )
+            string[] nq = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(nq[0]);
+            int q = Convert.ToInt32(nq[1]);
+            Dictionary<int, List<int>> seqList = new Dictionary<int, List<int>>(n);
+            int lastAns = 0;
+            
+            List<long> lastAnsList = new List<long>();
+            
+            //Trying to test with file input then start for loop from 1 instead of 0.
+            for ( int i = 0; i < q; i++ )
                 {
-                if ( index == 0 )
-                    {
-                    index++;
-                    continue;
-                    }
-                index++;
-                string[] splitQuery = query.Split(' ');
-                int queryType = Convert.ToInt32(splitQuery[0]);
-                long x = Convert.ToInt64(splitQuery[1]);
-                long y = Convert.ToInt64(splitQuery[2]);
+                //string[] query = lines[i].Split(' ');
+                string[] query = Console.ReadLine().Split(' ');
+                int queryType = Convert.ToInt32(query[0]);
+                int x = Convert.ToInt32(query[1]);
+                int y = Convert.ToInt32(query[2]);
 
-                long commonTerm = ((x ^ lastAnswer) % emptySequences);
+                int common = (x ^ lastAns) % n;
                 if ( queryType == 1 )
-                {
-                    long[] seq = seqList[commonTerm];
-                    if ( seq != null )
-                        lastIndex = seq.Length - 1;
-                    else
-                        {
-                        seqList[commonTerm] = new long[emptySequences];
-                        }
-                    seqList[commonTerm][lastIndex] = y;
-                    }
-                else
                     {
-                    long[] seq = seqList[commonTerm];
-                    if(seq != null)
-                    lastIndex = y % seq.Length;
-                    lastAnswerlist.Add(seq[lastIndex]);
-                    lastAnswer = seq[lastIndex];
+                    if ( !seqList.ContainsKey(common) )
+                        seqList.Add(common, new List<int>());
+
+                    seqList[common].Add(y);
+                    }
+                if ( queryType == 2 )
+                    {
+                    List<int> temp = new List<int>();
+                    if ( seqList.ContainsKey(common) )
+                        temp = seqList[common];
+
+                    lastAns = temp[y % temp.Count];
+                    lastAnsList.Add(lastAns);
                     }
                 }
-            foreach (var value in lastAnswerlist)
-                Console.WriteLine(value);
 
+            foreach ( int item in lastAnsList )
+                {
+                Console.WriteLine(item);
+                }
             Console.ReadLine();
             }
         }
