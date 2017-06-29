e5c42b32d1deace853aad53aa5dd301407db4ef8
prasadtelkikar <telkikar.prasad@gmail.com>
2017-06-29 23:45:21 +0530

    Solved program from inplementation: The Programmer Day

---
+++ Algorithms/Implementations/DayOfTheProgrammer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/* Sample Input 0:
+ * 2017
+ * Sample output 0:
+ * 13.09.2017
+ * Sample Input 1:
+ * 2016
+ * Sample output 1:
+ * 12.09.2016
+ */
+
+namespace Implementations
+    {
+    /// <summary>
+    /// Day of The Programmer (the 256th day of the year)
+    /// https://www.hackerrank.com/challenges/day-of-the-programmer/problem
+    /// </summary>
+    public class DayOfTheProgrammer
+        {
+        public static void Main (string[] args)
+            {
+            string yearString = Console.ReadLine();
+            int year = Convert.ToInt32(yearString);
+            if ( year < 1918 )
+                {
+                Console.WriteLine(year % 4 == 0 ? "12.09." + year : "13.09." + year);
+                }
+            else if ( year > 1918 )
+                {
+                Console.WriteLine((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) ? "12.09." + year : "13.09." + year);
+                }
+            else
+                {
+                Console.WriteLine("26.09."+year);
+                }
+            }
+        }
+    }
--- Algorithms/Stack/MaximumElements.cs
+++ Algorithms/Stack/MaximumElements.cs
@@ -10,33 +10,34 @@ namespace Stack
         {
         public static void Main (string[] args)
             {
-            int size = Convert.ToInt32(Console.ReadLine());
-            Stack<int> stack = new Stack<int>();
-            List<int> results = new List<int>();
+            Stack<long> stack = new Stack<long>();
+            int size = Convert.ToInt16(Console.ReadLine());
+            List<long> valueList = new List<long>();
             for ( int i = 0; i < size; i++ )
                 {
-                string[] input = Console.ReadLine().Split(' ');
-                int operation = Convert.ToInt32(input[0]);
-                switch ( operation )
+                string[] query = Console.ReadLine().Split(' ');
+                int type = Convert.ToInt32(query[0]);
+                
+                if ( type == 1 )
                     {
-                    case 1:
-                        int value = Convert.ToInt32(input[1]);
-                        stack.Push(value);
-                        break;
-                    case 2:
-                        int temp = stack.Pop();
-                        break;
-                    case 3:
-                        int popedValue = stack.Pop();
-                        results.Add(popedValue);
-                        break;
+                    long value = Convert.ToInt64(query[1]);
+                    stack.Push(value);
                     }
+                else if ( type == 2 )
+                    {
+                    stack.Pop();
+                    }
+                else
+                    {
+                    valueList.Add(stack.Peek());
+                    }
+
                 }
-            foreach ( int result in results )
+            foreach ( var item in valueList )
                 {
-                Console.WriteLine(result);
+                Console.WriteLine(item);
                 }
-            Console.ReadLine();
+            Console.ReadKey();
             }
         }
     }
