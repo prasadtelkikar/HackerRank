bc1c2546fb0472024f0338a91051a93ceb0e1a3a
prasadtelkikar <telkikar.prasad@gmail.com>
2017-07-17 23:20:46 +0530

    WOC34: first program 2 testcases failing

---
+++ Algorithms/Contests/Week of Code 34/OnceInTram.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contests.Week_of_Code_34
+    {
+    public class OnceInTram
+        {
+        public static void Main (string[] args)
+            {
+            string numString = Console.ReadLine();
+            int number = Convert.ToInt32(numString);
+            string firstSubNumber = numString.Substring(0, 3);
+            string secondSubNumber = numString.Substring(3, 3);
+            int firstNum = Convert.ToInt32(firstSubNumber);
+            int secondNum = Convert.ToInt32(secondSubNumber);
+
+            if ( firstNum != secondNum )
+                {
+                int sumFirst = GetSum(firstNum);
+                int sumSecond = GetSum(secondNum);
+                //Failing for 123456
+                //number += (sumFirst > sumSecond) ? (sumFirst - sumSecond) : (sumSecond - sumFirst);
+                }
+            else
+                {
+                int temp1 = firstNum % 100;
+                int temp2 = secondNum % 100;
+                    if (temp1 == 0 && temp2 == 0)
+                    {
+                        number = Convert.ToInt32(string.Concat((firstNum + 1).ToString(), (secondNum + 1).ToString()));
+                    }
+                    else
+                        number += 9;
+                }
+            Console.WriteLine(number);
+            Console.ReadKey();
+            }
+
+        private static int GetSum (int number)
+            {
+            int sum = 0;
+            while ( number > 0 )
+                {
+                sum += number % 10;
+                number /= 10;
+                }
+            return sum;
+            }
+        }
+    }
---
+++ Algorithms/Implementations/ChocolateFeast.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementations
+    {
+    public class ChocolateFeast
+        {
+        public static void Main (string[] args)
+            {
+            int trips = Convert.ToInt32(Console.ReadLine());
+            List<int> results = new List<int>(trips);
+            for ( int i = 0; i < trips; i++ )
+                {
+                string input = Console.ReadLine();
+                int result = GetTotalNumberOfChocolate(input);
+                results.Add(result);
+                }
+            foreach ( int result in results )
+                {
+                Console.WriteLine(result);
+                }
+            Console.ReadKey();
+            }
+
+        private static int GetTotalNumberOfChocolate (string input)
+            {
+            string[] inputs = input.Split(' ');
+            int dollers = Convert.ToInt32(inputs[0]);
+            int rateOfChocolate = Convert.ToInt32(inputs[1]);
+            int tradeRate = Convert.ToInt32(inputs[2]);
+            bool flag = true;
+            int chocolateCount = 0;
+            int remainingWrappers = 0;
+            do
+                {
+                if ( chocolateCount == 0 || chocolateCount >= tradeRate )
+                    {
+                    chocolateCount += dollers / rateOfChocolate;
+                    //remainingWrappers = chocolateCount % tradeRate;
+                    dollers = chocolateCount;
+                    rateOfChocolate = tradeRate;
+                    }
+                else
+                    flag = false;
+                } while ( flag );
+            return chocolateCount;
+            }
+        }
+    }
