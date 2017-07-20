a460fc93e70d31cb96218e8cea07f65d8b2aa5d8
prasadtelkikar <telkikar.prasad@gmail.com>
2017-07-20 22:28:45 +0530

    Solved easy program from HR and trying with second one

---
+++ Algorithms/Contests/Week of Code 34/MaxGcdAndSum.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contests.Week_of_Code_34
+    {
+    class MaxGcdAndSum
+        {
+        public static void Main (string[] args)
+            {
+            int size = Convert.ToInt32(Console.ReadLine());
+            string[] firstArrStr = Console.ReadLine().Split(' ');
+            string[] secondArrStr = Console.ReadLine().Split(' ');
+            int[] firstArr = Array.ConvertAll(firstArrStr, Int32.Parse);
+            int[] secondArr = Array.ConvertAll(secondArrStr, Int32.Parse);
+            int max = Int32.MinValue;
+            int sum = CalculateMaxGCD(firstArr, secondArr);
+            Console.WriteLine(sum);
+            Console.ReadLine();
+            }
+
+        private static int CalculateMaxGCD (int[] firstArr, int[] secondArr)
+            {
+            int finalResult = 0;
+            int max = Int32.MinValue;
+            int valueOfJthElement = 0;
+            for ( int i = 0; i < firstArr.Length; i++ )
+                {
+                int maxGCD = Int32.MinValue;
+                for ( int j = 0; j < firstArr.Length; j++ )
+                    {
+                    int result = GetGCD(firstArr[i], secondArr[j]);
+                    if ( maxGCD < result )
+                        {
+                        maxGCD = result;
+                        valueOfJthElement = secondArr[j];
+                        }
+                    }
+                if ( max < maxGCD )
+                    {
+                    finalResult = firstArr[i] + valueOfJthElement;
+                    }
+                }
+            return finalResult;
+            }
+
+        private static int GetGCD (int firstNumber, int secondNumber)
+            {
+            while ( true )
+                {
+                var remainder = firstNumber % secondNumber;
+
+                if ( remainder == 0 )
+                    return secondNumber;
+
+                firstNumber = secondNumber;
+                secondNumber = remainder;
+                }
+            }
+        }
+    }
--- Algorithms/Contests/Week of Code 34/OnceInTram.cs
+++ Algorithms/Contests/Week of Code 34/OnceInTram.cs
@@ -12,32 +12,36 @@ namespace Contests.Week_of_Code_34
             {
             string numString = Console.ReadLine();
             int number = Convert.ToInt32(numString);
-            string firstSubNumber = numString.Substring(0, 3);
-            string secondSubNumber = numString.Substring(3, 3);
-            int firstNum = Convert.ToInt32(firstSubNumber);
-            int secondNum = Convert.ToInt32(secondSubNumber);
-
-            if ( firstNum != secondNum )
-                {
-                int sumFirst = GetSum(firstNum);
-                int sumSecond = GetSum(secondNum);
-                //Failing for 123456
-                //number += (sumFirst > sumSecond) ? (sumFirst - sumSecond) : (sumSecond - sumFirst);
-                }
-            else
-                {
-                int temp1 = firstNum % 100;
-                int temp2 = secondNum % 100;
-                    if (temp1 == 0 && temp2 == 0)
-                    {
-                        number = Convert.ToInt32(string.Concat((firstNum + 1).ToString(), (secondNum + 1).ToString()));
-                    }
-                    else
-                        number += 9;
-                }
-            Console.WriteLine(number);
+            int result = GetNextLuckyNumber(number);
+            Console.WriteLine(result);
             Console.ReadKey();
             }
+
+        private static int GetNextLuckyNumber (int number)
+        {
+            while (true)
+            {
+                number += 1;
+                if (CheckSum(number))
+                    return number;
+            }
+        }
+
+        private static bool CheckSum(int number)
+        {
+            string numString = number.ToString();
+
+            int firstNum = Convert.ToInt32(numString.Substring(0, 3));
+            int secondNum = Convert.ToInt32(numString.Substring(3, 3));
+
+            int sumFirst = GetSum(firstNum);
+            int sumSecond = GetSum(secondNum);
+
+            if (sumFirst == sumSecond)
+                return true;
+
+            return false;
+        }
 
         private static int GetSum (int number)
             {
