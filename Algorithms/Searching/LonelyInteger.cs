ca0e542d3e5e587a40f13a505f8ef83db761c1f0
cls <telkikar.prasad@gmail.com>
2017-08-07 23:05:45 +0530

    Solved easy program

---
+++ Algorithms/Searching/LonelyInteger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+/* Sample input 0:
+ * 1
+ * 1
+ * Sample output 0:
+ * 1
+ * Sample input 1:
+ * 3
+ * 1 1 2
+ * Sample output 1:
+ * 2
+ * Sample input 2:
+ * 5
+ * 0 0 1 2 1
+ * Sample output 2:
+ * 2
+ * Sample input 3:
+ * 9
+ * 4 9 95 93 57 4 57 93 9
+ * Sample output 3:
+ * 95
+ */
+namespace Searching
+    {
+    /// <summary>
+    /// Lonely Integer
+    /// https://www.hackerrank.com/challenges/lonely-integer
+    /// </summary>
+    public class LonelyInteger
+        {
+        public static void Main (string[] args)
+            {
+            int limit = Convert.ToInt32(Console.ReadLine());
+            string[] stringArr = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(stringArr, int.Parse);
+            int[] resultCount = new int[101];
+            int result = 0;
+            for ( int i = 0; i < limit; i++ )
+                resultCount[arr[i]]++;
+
+            for ( int i = 0; i < limit; i++ )
+                if ( resultCount[arr[i]] == 1 )
+                    {
+                    result = arr[i];
+                    break;
+                    }
+            Console.WriteLine(result);
+            Console.ReadKey();
+            }
+        }
+    }
---
+++ Algorithms/Stack/GameOfTwoStacks.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+    {
+    public class GameOfTwoStacks
+        {
+        public static void Main (string[] args)
+            {
+            int gameCount = Convert.ToInt32(Console.ReadLine());
+            while ( gameCount-- != 0 )
+                {
+                string[] inputs = Console.ReadLine().Split(' ');
+                int firstStackSize = Convert.ToInt32(inputs[0]);
+                int secondStackSize = Convert.ToInt32(inputs[1]);
+                int limit = Convert.ToInt32(inputs[2]);
+                Stack<int> firstStack = new Stack<int>();
+                Stack<int> secondStack = new Stack<int>();
+                string[] firstInput = Console.ReadLine().Split(' ');
+                string[] secondInput = Console.ReadLine().Split(' ');
+
+                for ( int i = firstStackSize - 1; i >= 0; i-- )
+                    firstStack.Push(Convert.ToInt32(firstInput[i]));
+
+                for ( int i = secondStackSize - 1; i >= 0; i-- )
+                    secondStack.Push(Convert.ToInt32(secondInput[i]));
+                
+                int sum = 0;
+                int count = 0;
+                while ( sum < limit )
+                    {
+                    int first = 0;
+                    int second = 0;
+                    if(firstStack.Count != 0)
+                        first = firstStack.Peek();
+                    if(secondStack.Count != 0)
+                        second = secondStack.Peek();
+
+                    if (first != 0 && first < second )
+                        {
+                        sum += first;
+                        firstStack.Pop();
+                        }
+                    else
+                        {
+                        if ( second != 0 )
+                            {
+                            sum += second;
+                            secondStack.Pop();
+                            }
+                        }
+                    if ( sum > limit || (firstStack == null && secondStack == null))
+                        break;
+
+                    count++;
+                    }
+                Console.WriteLine(count);
+                Console.ReadKey();
+                }
+            }
+        }
+    }
