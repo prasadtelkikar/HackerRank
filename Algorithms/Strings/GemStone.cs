462169efc60e7d653c90b16de0427ff2d2a02a84
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-01-23 23:52:48 +0530

    Started GemStone program

---
+++ Algorithms/Strings/GemStone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class GemStone
+    {
+        public static void Main(string[] args)
+        {
+            int inputs = Convert.ToInt32(Console.ReadLine());
+            Dictionary<char, int> countLetters = new Dictionary<char, int>();
+            string aToz = "abcdefghijklmnopqrstuvwxyz";
+
+            foreach (char ch in aToz)
+                countLetters.Add(ch, 0);
+
+            for (int i = 0; i < inputs; i++)
+            {
+                string inputString = Console.ReadLine();
+                foreach (char chq in inputString)
+                {
+                    if(countLetters.ContainsKey(chq))
+                        countLetters[chq]++;
+                }
+                foreach (char chq in aToz)
+                {
+                    if (countLetters.ContainsKey(chq) && countLetters[chq] != i + 1)
+                        countLetters.Remove(chq);
+                }
+            }
+
+
+            Console.WriteLine(countLetters.Count);
+            Console.ReadKey();
+        }
+    }
+}
