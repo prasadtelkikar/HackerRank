6b2e33d92818a38aac0f12a59e0320b26c36190a
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-01-25 23:38:46 +0530

    Solved LoveLetterMystery and Pangram program from HR

---
+++ Algorithms/Strings/LoveLetterMystery.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strings
+{
+    class LoveLetterMystery
+    {
+        public static void Main(string[] args)
+        {
+            int testCases = Convert.ToInt32(Console.ReadLine());
+            List<int> resultList = new List<int>();
+            for (int i = 0; i < testCases; i++)
+            {
+                string input = Console.ReadLine();
+                int result = 0;
+                result = GetResult(input, (input.Length / 2));
+
+                resultList.Add(result);
+            }
+
+            foreach (int item in resultList)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+
+        public static int GetResult(string input, int midIndex)
+        {
+            int result = 0;
+            string firstString = input.Substring(0, midIndex);
+            string secondString = input.Substring(midIndex);
+            string reverseString = "";
+            int len = secondString.Length - 1;
+
+            while (len >= 0)
+            {
+                reverseString = reverseString + secondString[len];
+                len--;
+            }
+
+            for (int j = 0; j < firstString.Length; j++)
+                result += firstString[j] > reverseString[j] ? Convert.ToInt32(firstString[j] - reverseString[j]) : Convert.ToInt32(reverseString[j] - firstString[j]);
+            return result;
+        }
+    }
+}
---
+++ Algorithms/Strings/Pangram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strings
+{
+    class Pangram
+    {
+        public static void Main(string[] args)
+        {
+            bool result = IsPangram();
+            Console.WriteLine(result ? "pangram" : "not pangram");
+            Console.ReadKey();
+        }
+
+        private static bool IsPangram()
+        {
+            string aToz = "abcdefghijklmnopqrstuvwxyz ";
+            Dictionary<char, bool> dict = new Dictionary<char, bool>();
+
+            foreach (char item in aToz)
+                dict.Add(item, false);
+
+            string input = Console.ReadLine().ToLower() ;
+
+            foreach (char item in input)
+            {
+                if (!dict[item])
+                    dict[item] = true;
+            }
+
+            foreach (var item in dict.Keys)
+            {
+                if (!dict[item])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
