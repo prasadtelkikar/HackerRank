c46578162f0133980b9144d47c5bcc59bdb4332a
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-02-24 23:34:10 +0530

    Added comment

--- Algorithms/Implementations/NonDivisibleSubset.cs
+++ Algorithms/Implementations/NonDivisibleSubset.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//TODO: Incomplete.
+//TODO: Incomplete.Do it tomorrow...25/02/2018
 namespace Implementations
 {
     class NonDivisibleSubset
