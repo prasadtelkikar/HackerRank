634c9896308d05ccdc265e0fb94baaf0055b358b
prasadtelkikar <telkikar.prasad@gmail.com>
2019-03-15 23:02:23 +0530

    Started with Ransom notes

---
+++ Algorithms/DictionaryAndHashTables/RansomNotes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAndHashTables
+{
+    public class RansomNotes
+    {
+        public static void Main(string[] args)
+        {
+            string inputLength = Console.ReadLine();
+            List<string> firstSentenceArray = Console.ReadLine().Split(' ').ToList();
+            List<string> secondSentenceArray = Console.ReadLine().Split(' ').ToList();
+
+            var result = secondSentenceArray.Intersect(firstSentenceArray).ToList();
+
+            Console.WriteLine(result.Count == secondSentenceArray.Count ? "Yes" : "No");
+            Console.ReadKey();
+        }
+    }
+}
