492f85f826ef9bd92bd1b89aef6b2f595ae82f0a
prasadtelkikar <telkikar.prasad@gmail.com>
2019-03-18 23:50:20 +0530

    Solved Ransom note program . Looked into discussion chart

--- Algorithms/DictionaryAndHashTables/RansomNotes.cs
+++ Algorithms/DictionaryAndHashTables/RansomNotes.cs
@@ -11,13 +11,42 @@ namespace DictionaryAndHashTables
         public static void Main(string[] args)
         {
             string inputLength = Console.ReadLine();
-            List<string> firstSentenceArray = Console.ReadLine().Split(' ').ToList();
-            List<string> secondSentenceArray = Console.ReadLine().Split(' ').ToList();
+            List<string> magazine = Console.ReadLine().Split(' ').OrderBy(x => x).ToList();
+            List<string> note = Console.ReadLine().Split(' ').OrderBy(x => x).ToList();
+            bool resultBool = true;
 
-            var result = secondSentenceArray.Intersect(firstSentenceArray).ToList();
+            var magazineLength = magazine.Count;
+            var noteLength = note.Count;
 
-            Console.WriteLine(result.Count == secondSentenceArray.Count ? "Yes" : "No");
+            if (noteLength > magazineLength)
+                resultBool = false;
+            else
+            {
+                int i = 0, j = 0, count = 0;
+                while(i < magazineLength && j < noteLength)
+                {
+                    if(magazine[i] == note[j])
+                    {
+                        count++;
+                        j++;
+                    }
+                    i++;
+                }
+                resultBool = count == noteLength;
+            }
+
+            Console.WriteLine(resultBool ? "Yes" : "No");
             Console.ReadKey();
         }
     }
 }
+
+
+/* My attempts
+ * 
+ *     // All passed except 3 test cases
+ *     static void checkMagazine(List<string> magazine, List<string> note) {
+ *          List<string> result = note.Intersect(magazine).ToList();
+ *          Console.WriteLine(result.Count == note.Count ? "Yes" : "No");
+ *    }
+ */
